ad6f2b5064da58f4c5ad378f7ad6a6e65c96e7fb
Luis Berenguer <luis01developer@gmail.com>
2021-10-16 11:59:18 +0200

    Fix order migration, problem with virtualization

--- db/migrations/20211015115630-order.js
+++ db/migrations/20211015115630-order.js
@@ -1,10 +1,35 @@
 'use strict';
-
-const { ORDER_TABLE, OrderSchema } = require('../models/orderModel')
+const { CUSTOMER_TABLE } = require('./customerModel')
+const { DataTypes, Sequelize } = require('sequelize');
+const { ORDER_TABLE } = require('../models/orderModel')
 
 module.exports = {
   up: async (queryInterface) => {
-    await queryInterface.createTable(ORDER_TABLE, OrderSchema);
+    await queryInterface.createTable(ORDER_TABLE, {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: DataTypes.INTEGER
+      },
+      customerId: {
+        field: 'customer_id',
+        allowNull: false,
+        type: DataTypes.INTEGER,
+        references: {
+          model: CUSTOMER_TABLE,
+          key: 'id'
+        },
+        onUpdate: 'CASCADE',
+        onDelete: 'SET NULL'
+          },
+      createdAt: {
+        allowNull: false,
+        type: DataTypes.DATE,
+        field: 'created_at',
+        defaultValue: Sequelize.NOW,
+        },
+    });
   },
 
   down: async (queryInterface) => {

8c42572c1f8ad1ffcf197c23303169c4e5532b1d
Luis Berenguer <luis01developer@gmail.com>
2021-10-16 12:02:23 +0200

    Fix profuct and categories migration

--- db/migrations/20211015085707-products.js
+++ db/migrations/20211015085707-products.js
@@ -5,12 +5,12 @@ const { PRODUCT_TABLE, ProductSchema } = require('../models/productModel')
 
 module.exports = {
   up: async (queryInterface) => {
-    await queryInterface.createTable(CATEGORY_TABLE, CategorySchema);
     await queryInterface.createTable(PRODUCT_TABLE, ProductSchema);
+    await queryInterface.createTable(CATEGORY_TABLE, CategorySchema);
   },
 
   down: async (queryInterface) => {
-    await queryInterface.dropTable(CATEGORY_TABLE);
     await queryInterface.dropTable(PRODUCT_TABLE);
+    await queryInterface.dropTable(CATEGORY_TABLE);
   }
 };
